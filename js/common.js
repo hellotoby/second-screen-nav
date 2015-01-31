82974e9fba5fb12efdd800e1f30fdf396ab78493
Toby Vervaart <hello@tobyvervaart.com.au>
2015-02-01 10:27:03 +1100

    Removed hard coded url for client socket connection

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 
 // Change page when we get a url sent from the second screen
-var socket = io.connect('http://localhost:8000');
+var socket = io.connect();
 socket.on('urlchange', function (data) {
     window.location.href = data.url;
 });
